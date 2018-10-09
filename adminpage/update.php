bbe732fa79feb82a6416995b09d1bbdea1ad5371
Avik <aviknandi@yahoo.com>
2018-10-09 22:13:05 +0530

    created update first from to show which data user want to update

--- adminpage/update.php
+++ adminpage/update.php
@@ -1,1 +1,27 @@
-<?php  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Updtae Student Details</title>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <!-- style.css -->
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>
+    <div class="main-text-container insertBox">
+      <form class="updateForm" action="update.php" method="post">
+        <div class="form-group">
+          <label>Student Name:</label>
+          <input type="text" class="form-control student-name" name="updateStudentName" required>
+          <label>Student Class:</label>
+          <input type="text" class="form-control student-class" name="updateStudentClass" required>
+          <input type="submit" name="checkForUpdate" value="Search" class="updateSubmit">
+        </div>
+      </form>
+    </div>
+
+  </body>
+</html>
